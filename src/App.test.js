489bfb21edeec2352e3284a339101cf4dda2d085
David Gasana Manzi <gmdavid59@gmail.com>
2021-07-11 10:24:52 +0200

    Added a header div

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,19 @@
-test('simple passing test', () => {
-  expect(true).toBe(true)
-});
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+import toJson from "enzyme-to-json"
+
+describe('App', () => {
+  let app
+  beforeEach(() => app = shallow(<App/>))
+  it('renders correctly', () => {
+    expect(toJson(app)).toMatchSnapshot()
+  });
+
+  it('has a header', () => {
+    expect(app.find('.header').exists()).toBe(true)
+  });
+
+  
+})
+
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-        <div className="header">
-            <h1>Welcome to TicTacToe Game</h1>
-        </div>
-    )
-}
-
-export default Header
